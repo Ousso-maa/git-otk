6dc9527d5c57fd2e2be5cce2078df3a236fa4722
Ousso-maa <othmansidika@gmail.com>
2024-02-14 17:01:25 +0000

    Suppression de devoir.js

--- devoir.js
+++
@@ -1,7 +0,0 @@
-function changer(){
-    var leftid = document.getElementById('pic_lef');
-    rightid = document.getElementById('pic_rigth');
-    leftSRC = leftid.scr;
-    rightSRC = right.scr;
-leftid.scr = rightSRC;
-}
